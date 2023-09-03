51fe42383e7d501a6a62b1fd3a4806140c3dbf57
unknown <mariamatef226@gmail.com>
2023-09-03 14:07:53 +0300

    validation is added

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # Temperature Converter 🌡️
 
+
+## Table of Content:
+
+- [Project Description](#project-description)
+- [Demo](#demo)
+- [Technologies](#technologies)
+- [Pre-requisites](#pre-requisites)
+- [Setup](#setup)
+- [Approach](#approach)
+- [License](#license)
+- [Author](#author)
+
+## Project Description :page_facing_up:
+
+Temperature Converter is a simple React.js app that takes user input for a specific temperature in a specific measuring unit, and converts it to other units. Three measuring units are included: Celsius, Fahrenheit and Kelvin. In case of non-numeric input, conversion is prohibited and an alert message is displayed. The application is responsive on all devices.
+
+## Demo
+
+`![Writing](https://unsplash.com/photos/VBPzRgd7gfc)`
+
+Picture by [Kelly Sikkema](https://unsplash.com/@kellysikkema)
+
+## Technologies 💻
+
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="react" width="30" height="30"> __React.js__
+
+## Pre-requisites :grey_question:
+
+- Node.js (version 14 or higher)
+- npm (version 6 or higher)
+- React 18 is used
+
+## Setup ⬇️
+
+1. Clone the repository or download the source code.
+
+2. Navigate to the project directory using your terminal.
+
+```
+cd random-quotes-app
+```
+
+3. Install the required dependencies.
+
+```
+npm install
+```
+
+4. Start the development server.
+
+```
+npm start
+```
+
+5. Open your web browser and navigate to http://localhost:3000 to see the app in action.
+
+
+## Approach
+I adopted the `BEM` naming style for my css class names and ...
+
+
+## License ⚖️
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Author 🖊️
+
+**Mariam Atef Hassan  - October 2022**
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Result from './components/Result'
-import { useState, useEffect } from 'react'
+import Result from "./components/Result";
+import { useState } from "react";
 
 function App() {
-  let [converted, setConverted] = useState({ celsius: 0, fahrenheit: 32, kelvin: 273.15 });
+  let [converted, setConverted] = useState({
+    celsius: 0,
+    fahrenheit: 32,
+    kelvin: 273.15,
+  });
   let [temperature, setTemperature] = useState(0); // for single source of truth and form control management
-  let [currentUnit, setCurrentUnit] = useState('c'); // for single source of truth and form control management
+  let [currentUnit, setCurrentUnit] = useState("c"); // for single source of truth and form control management
 
-
+  // Handle Form control changes to reflect in react states for single source of truth
   function tempChange(event) {
     let value = event.target.value;
     setTemperature(value);
@@ -17,78 +21,105 @@ function App() {
     setCurrentUnit(value);
   }
 
+  // unit conversion functions
   function celsiusToFahrenheit(temp) {
-    return (temp * 9 / 5 + 32);
+    return (temp * 9) / 5 + 32;
   }
 
   function fahrenheitToKelvin(temp) {
-    return ((temp - 32) * 5 / 9 + 273.15)
+    return ((temp - 32) * 5) / 9 + 273.15;
   }
 
   function kelvinToCelsius(temp) {
-    return (temp - 273.15)
+    return temp - 273.15;
   }
 
+  // conversion logic
   function convert() {
-    switch (currentUnit) {
-      case 'c':
-        setConverted((prev) => {
-          let f = celsiusToFahrenheit(temperature);
-          let k = fahrenheitToKelvin(f);
-          return { celsius: temperature, fahrenheit: f, kelvin: k }
-        });
-        break;
-      case 'f':
-        setConverted((prev) => {
-          let k = fahrenheitToKelvin(temperature);
-          let c = kelvinToCelsius(k);
-          return { celsius: c, fahrenheit: temperature, kelvin: k }
-        });
+    // check if valid convertion
+    if (!isNaN(temperature)) {
+      switch (currentUnit) {
+        case "c":
+          setConverted((prev) => {
+            let f = celsiusToFahrenheit(temperature);
+            let k = fahrenheitToKelvin(f);
+            return { celsius: temperature, fahrenheit: f, kelvin: k };
+          });
+          break;
+        case "f":
+          setConverted((prev) => {
+            let k = fahrenheitToKelvin(temperature);
+            let c = kelvinToCelsius(k);
+            return { celsius: c, fahrenheit: temperature, kelvin: k };
+          });
 
-        break;
-      case 'k':
-        setConverted((prev) => {
-          let c = kelvinToCelsius(temperature);
-          let f = celsiusToFahrenheit(c);
-          return { celsius: c, fahrenheit: f, kelvin: temperature }
-        });
+          break;
+        case "k":
+          setConverted((prev) => {
+            let c = kelvinToCelsius(temperature);
+            let f = celsiusToFahrenheit(c);
+            return { celsius: c, fahrenheit: f, kelvin: temperature };
+          });
 
-        break;
-
+          break;
+      }
     }
-
   }
-
 
   return (
     <>
       <div className="converter-container p-4 text-center">
-        <h1 className="text-center mt-4">Temperature Converter</h1>
+        <h1 className="text-center mt-4 fw-bold">Temperature Converter</h1>
+
         <div className="d-flex mt-4">
-          <input value={temperature} className="w-75 p-2 clear-outline" onChange={tempChange} />
-          <select name='unit' className="w-25 text-light p-2 clear-outline" onChange={currentUnitChange}>
-            <option selected={currentUnit === 'c'} value='c'>
+          <input
+            value={temperature}
+            className="p-2 clear-outline"
+            onChange={tempChange}
+          />
+          <select
+            name="unit"
+            className="text-light p-2 clear-outline"
+            onChange={currentUnitChange}
+          >
+            <option selected={currentUnit === "c"} value="c">
               Celsius
             </option>
-            <option selected={currentUnit === 'f'} value='f'>
+            <option selected={currentUnit === "f"} value="f">
               Fahrenheit
             </option>
-            <option selected={currentUnit === 'k'} value='k'>
+            <option selected={currentUnit === "k"} value="k">
               Kelvin
             </option>
           </select>
         </div>
-        <button className='clear-outline p-2 m-2 mt-4 convert' onClick={convert}>Convert</button>
+
+        <div
+          className={`text-danger caution ${
+            !isNaN(temperature) && "invisible"
+          }`}
+        >
+          Input temperature should be a valid number!
+        </div>
+
+        <button
+          className={`clear-outline p-2 m-2 mt-2 convert ${
+            !isNaN(temperature) && "enable"
+          }`}
+          onClick={convert}
+          disabled={isNaN(temperature)}
+        >
+          Convert
+        </button>
 
         <div className="results pb-4">
-          <Result unit="Celsius" value={converted.celsius} unitSym='°C' />
-          <Result unit="Fahrenheit" value={converted.fahrenheit} unitSym='°F' />
-          <Result unit="Kelvin" value={converted.kelvin} unitSym='°K' />
-
+          <Result unit="Celsius" value={converted.celsius} unitSym="°C" />
+          <Result unit="Fahrenheit" value={converted.fahrenheit} unitSym="°F" />
+          <Result unit="Kelvin" value={converted.kelvin} unitSym="°K" />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
